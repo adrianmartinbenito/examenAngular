ebc203b50cacbfbc9f05b3c4153e0d5614694d44
adrianmartin <adrianmartin@devanddel.com>
2022-04-04 10:54:06 +0200

    Finalize

--- examen/src/app/features/product/product.component.ts
+++ examen/src/app/features/product/product.component.ts
@@ -9,18 +9,18 @@ export class ProductComponent implements OnInit {
 
   products = [
     {
-      "name": "Palmerita1",
-      "url": "https://prod-mercadona.imgix.net/images/5e949a6f64b52be230b8f1084156dcd9.jpg?fit=crop&h=300&w=300"
+      name: "Palmerita1",
+      url: "https://prod-mercadona.imgix.net/images/5e949a6f64b52be230b8f1084156dcd9.jpg?fit=crop&h=300&w=300"
 
     },
     {
-      "name": "Palmerita2",
-      "url": "https://prod-mercadona.imgix.net/images/5e949a6f64b52be230b8f1084156dcd9.jpg?fit=crop&h=300&w=300"
+      name: "Palmerita2",
+      url: "https://prod-mercadona.imgix.net/images/5e949a6f64b52be230b8f1084156dcd9.jpg?fit=crop&h=300&w=300"
 
     },
     {
-      "name": "Palmerita3",
-      "url": "https://prod-mercadona.imgix.net/images/5e949a6f64b52be230b8f1084156dcd9.jpg?fit=crop&h=300&w=300"
+      name: "Palmerita3",
+      url: "https://prod-mercadona.imgix.net/images/5e949a6f64b52be230b8f1084156dcd9.jpg?fit=crop&h=300&w=300"
 
     }
   ]
